50471660a98899ccc771fd3ac815b04c0a3fc20c
Moshe Zada <zada.moshe@gmail.com>
2018-08-29 16:32:40 +0200

    Add TLS support

--- src/plugins/redis.js
+++ src/plugins/redis.js
@@ -6,10 +6,11 @@ module.exports = (program) => {
     .command('redis <list>')
     .description('Monitor redis list length')
     .alias('r')
-    .option('-i, --pollingInterval [n]', 'Set the polling interval for to command argument', 50)
+    .option('-i, --pollingInterval [n]', 'Set the polling interval', 50)
     .option('-h, --host [name]', 'Connect to remote host', 'localhost')
+    .option('-t, --enableTLS', 'Set up a TLS connection to Redis', false)
     .action((list, options) => {
-      const client = redis.createClient({ host: options.host });
+      const client = redis.createClient({ host: options.host, tls: options.enableTLS });
       const basicGrid = new BasicGrid(options.parent);
 
       function pollCommand() {
